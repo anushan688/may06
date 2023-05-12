9defe566ecbb2cd9254c72e45dd95c5752ba20fa
anushan688 <131831427+anushan688@users.noreply.github.com>
2023-05-11 19:05:12 -0500

    Initial project

---
+++ mykatalone/src/main/java/com/mycompany/mykatalone/Mykatalone.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mykatalone;
+
+/**
+ *
+ * @author suman
+ */
+public class Mykatalone {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
